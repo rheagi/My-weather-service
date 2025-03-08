9fa5317d017615f1477907cec04266e860151b73
rheagi <1388500@gmail.com>
2025-03-08 21:57:33 +0100

    Made class for Coffee Maker (1st version)

---
+++ MyFirstProject/ClassesLesson/Cat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLesson
+{
+    internal class Cat
+    {
+        private string _name;
+        
+        public string Name
+        {
+            get
+            {
+                Console.WriteLine("Ura");
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException();
+                }
+                _name = value;
+            }
+        }
+
+        public int Height { get; private set; }
+        public int Weight { get; set; }
+        public string Breed { get; set; }
+        public int Sex { get; set; }
+
+        public Cat()
+        { }
+        
+        public Cat(string name)
+        {
+            Name = name;
+        }
+
+        public Cat(string name, string breed)
+        {
+            Name = name;
+            Breed = breed;
+        }
+    }
+}
---
+++ MyFirstProject/ClassesLesson/CoffeeMaker.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLesson
+{
+    internal class CoffeeMaker
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("This is Coffee Maker.\n");
+            Console.WriteLine("We have espresso, americano, cappuccino, latte and mocha.\n");
+            Console.Write("Choose what you want: ");
+        }
+
+        public Cup MakeCoffee(string choice)
+        {
+            Cup drink;
+
+            switch (choice)
+            {
+                case "espresso":
+                    drink = MakeEspresso();
+                    break;
+                case "americano":
+                    drink = MakeAmericano();
+                    break;
+                case "cappuccino":
+                    drink = MakeCappuccino();
+                    break;
+                case "latte":
+                    drink = MakeLatte();
+                    break;
+                case "mocha":
+                    drink = MakeMocha();
+                    break;
+                default:
+                    Console.Write("\nMistake! Try again: ");
+                    return null;
+            }
+
+            Console.WriteLine($"\nYour {choice} is ready. Enjoy!");
+            return drink;
+
+        }
+
+
+        private Cup MakeEspresso()
+        {
+            Cup espressoCup = SetCup();
+            AddCoffee(espressoCup);
+            return espressoCup;
+        }
+
+        private Cup MakeAmericano()
+        {
+            Cup americanoCup = SetCup();
+            AddCoffee(americanoCup);
+            AddWater(americanoCup);
+            return americanoCup;
+        }
+
+        private Cup MakeCappuccino()
+        {
+            Cup cappuccinoCup = SetCup();
+            AddCoffee(cappuccinoCup);
+            AddMilk(cappuccinoCup);
+            return cappuccinoCup;
+        }
+
+        private Cup MakeLatte()
+        {
+            Cup latteCup = SetCup();
+            AddCoffee(latteCup);
+            AddMilk(latteCup);
+            AddMilk(latteCup);
+            return latteCup;
+        }
+
+        private Cup MakeMocha()
+        {
+            Cup mochaCup = SetCup();
+            AddCoffee(mochaCup);
+            AddMilk(mochaCup);
+            AddChocolate(mochaCup);
+            return mochaCup;
+        }
+
+        private Cup SetCup()
+        {
+            return new Cup();
+        }
+
+        private void AddCoffee(Cup cup)
+        {
+            Console.WriteLine("Adding coffee...");
+            cup.Content.Add("coffee");
+        }
+
+        private void AddMilk(Cup cup)
+        {
+            Console.WriteLine("Adding milk...");
+            cup.Content.Add("milk");
+        }
+
+        private void AddWater(Cup cup)
+        {
+            Console.WriteLine("Adding water...");
+            cup.Content.Add("water");
+        }
+
+        private void AddChocolate(Cup cup)
+        {
+            Console.WriteLine("Adding chocolate...");
+            cup.Content.Add("chocolate");
+        }
+    }
+}
