cee08db47d5bbd273939e6b1c469f622fdb8e9b2
rheagi <1388500@gmail.com>
2025-03-09 22:32:14 +0100

    Made second version of Coffee Maker

---
+++ MyFirstProject/ClassesLesson/Recipes/!CoffeeRecipe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLesson.Recipes
+{
+    public abstract class CoffeeRecipe
+    {
+        protected void AddCoffee(Cup cup)
+        {
+            Console.WriteLine("Adding coffee...");
+            cup.Content.Add("coffee");
+        }
+
+        protected void AddMilk(Cup cup)
+        {
+            Console.WriteLine("Adding milk...");
+            cup.Content.Add("milk");
+        }
+
+        protected void AddWater(Cup cup)
+        {
+            Console.WriteLine("Adding water...");
+            cup.Content.Add("water");
+        }
+
+        protected void AddChocolate(Cup cup)
+        {
+            Console.WriteLine("Adding chocolate...");
+            cup.Content.Add("chocolate");
+        }
+
+        public abstract void MakeCoffee(Cup cup);
+    }
+}
---
+++ MyFirstProject/ClassesLesson/Recipes/EspressoRecipe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLesson.Recipes
+{
+    internal class EspressoRecipe : CoffeeRecipe
+    {
+        public override void MakeCoffee(Cup cup)
+        {
+            AddCoffee(cup);
+        }
+    }
+}
